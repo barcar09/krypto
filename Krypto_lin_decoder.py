e673950a7cb381de159c73589f5980539816199d
bartek <bartek@DESKTOP-L4V8KIN>
2018-03-21 17:44:30 +0100

    More cosmetic changes

--- Krypto_lin_decoder.py
+++ Krypto_lin_decoder.py
@@ -21,23 +21,29 @@ def mulinv(b, n):   # Multi modulo inversion function
     g, x, _ = xgcd(b, n)
     if g == 1:
         return x % n
+
 def GCD(a,b):   # Great common dividor function
 	while a != 0:
 			a,b = b % a, a
 	return b
+
 def m_returner(list_of_el):     # Function returning m in (a*x +c) % m function 
     t = 0
     empty_sum_list = [s1 - s0 for s0, s1 in zip(list_of_el, list_of_el[1:])]
     u_list = [t2*t0 - t1*t1 for t0, t1, t2 in zip(empty_sum_list, empty_sum_list[1:], empty_sum_list[2:])]
     soln =  abs(reduce(GCD,u_list))
     return soln
+
 def a_returner(list_of_el, modulus):     # Function returning a in (a*x +c) % m function 
     multiplier = (list_of_el[2] - list_of_el[1]) * mulinv(list_of_el[1] - list_of_el[0], modulus) % modulus
     # we use multi inverse a*x % m = 1,
     return multiplier
+
 def c_returner(list_of_el,modulus,multiplier):   # Function returning c in (a*x +c) % m function 
      increment = (list_of_el[1] - list_of_el[0]*multiplier) % modulus
      return(increment)
+
+
 for el in range(100):   # Simple test's
     list_of_el = linear_congruential_generator(67225,7382843,10,4324234)
     est=m_returner(list_of_el)
